fb39ccc37d726222edc10233a38624bf9ae37e8f
Fabricio <fabricioalvesfragoso@gmail.com>
2024-09-16 20:11:35 -0300

    Calculadora

---
+++ Calculadora/src/main/java/com/mycompany/calculadora/Calculadora.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadora;
+
+/**
+ *
+ * @author Fabrício
+ */
+public class Calculadora {
+
+    public static void main(String[] args) {
+        Tela form = new Tela();    
+        form.setVisible(true);
+    }
+}
---
+++ ProjetoCalculadora/src/main/java/com/mycompany/projetocalculadora/ProjetoCalculadora.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetocalculadora;
+
+/**
+ *
+ * @author Fabrício
+ */
+public class ProjetoCalculadora {
+
+    public static void main(String[] args) {
+      Tela form = new Tela();    
+      form.setVisible(true);
+    }
+}
